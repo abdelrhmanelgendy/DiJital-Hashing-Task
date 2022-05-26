3ca50e6e5f709af123a45f9dae143e58e1544cf0
Abd ElRahman Mosad <development3@teztour.com.eg>
2022-05-26 11:40:25 +0200

    creating the project layers

---
+++ domain/src/main/java/com/tasks/domain/model/HashDataRequest.kt
@@ -0,0 +1,5 @@
+package com.info.tasks.domain.model
+
+data class HashDataRequest(
+    val GUID: String
+)
---
+++ domain/src/main/java/com/tasks/domain/model/HashDataResponse.kt
@@ -0,0 +1,7 @@
+package com.info.tasks.domain.model
+
+data class HashDataResponse(
+    val email: String,
+    val hash: String,
+    val name: String
+)
